c39d7b438c18c90072a9e860ebadddaddeab1b3f
gannicus.teo <gannicus4431@gmail.com>
2018-03-10 02:03:24 +0800

    clean up stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-
-let defaultTextColor = '#gff';
 let defaultStyle = {
-  color: defaultTextColor
+  color: '#gff'
 };
 
 class Aggregate extends Component {
@@ -41,12 +38,11 @@ class Playlist extends Component {
   );
   }
 }
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Title</h1>
+        <h1 style = {{...defaultStyle,'font-size':'54px'}}>Title</h1>
           <Aggregate/>
           <Aggregate/>
           <Filter/>
